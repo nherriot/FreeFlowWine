dbcf4460df238a525f4b0eec8ba4767f82879b5f
DilshadGit <dilshad.abdulla@outlook.com>
2016-09-27 23:57:24 +0100

    Create admin dashboard and some extra links such as setting, login and register

--- ffwproject/cms/urls.py
+++ ffwproject/cms/urls.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 
-from cms.views import index_page
+from cms.views import index_page, dashboard_page
 
 
 urlpatterns = [
-	url('^$', index_page, name='index'),
+    url('^$', index_page, name='index'),
+  	url('^dashboard/$', dashboard_page, name='admin_page'),  
 ]
