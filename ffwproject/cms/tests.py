05f4908c370d010fe771607c34a4f6a0d214e933
Nicholas Herriot <Nicholas.Herriot@vodafone.com>
2016-11-29 22:23:57 +0000

    Adding fixtures to store data for 'catalog' test cases.

--- ffwproject/cms/tests.py
+++ ffwproject/cms/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase, Client
 from django.core import urlresolvers
 import httplib
 from django.contrib.auth import SESSION_KEY
+from catalog.models import Category
 
 # Create your tests here.
 
@@ -61,8 +62,45 @@ class ExampleTestCase(TestCase):
     def test_setup_method_2(self):
         print "Running test_setup_method_2"
         print "Check that my_value is: ", self.my_value
-        self.assertEquals(self.my_value, 1)
-        self.failIfEqual(self.my_value, 0)
+        self.assertEquals(self.my_value, 0)
+        self.failIfEqual(self.my_value, 1)
+
+
+
+class NewUserTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        
+    def test_view_catalog(self):
+        print "We are viewing a 'catalog' stored in the database"
+
+        # Lets fetch values from our model first that we wish to check
+        category = Category.active.all()[0]
+        category_url = category.get_absolute_url()
+
+        # Lets test the loading of our category page
+        response = self.client.get(category_url)
+
+        # now test that we get a response for that url
+        print "The category URL is:", category_url
+
+
+        # We need to check that we did get a response from our HTTP GET to the admin page.
+        self.failUnless(response)
+
+        # OK So far so good. Check the response matches an http 200 which is (httplib.OK = 200)
+        self.assertEquals(response.status_code, httplib.OK)
+
+
+
+
+
+
+
+
+
+
 
 
 
